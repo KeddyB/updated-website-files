cca8ba6422c4cc9f45d8268c91dec8af9b527605
KeddyB <bokelusi@gmail.com>
2023-01-17 01:02:30 +0100

    day 30+ commit

--- app.js
+++ app.js
@@ -36,19 +36,16 @@ themeBtn.addEventListener('click', ()=>{
 })
 pageTransition()
 
-const cookieBox = document.querySelector(".wrapper"),
-acceptBtn = cookieBox.querySelector(".buttons button")
+const cookieBox = document.querySelector(".wrapper");
+acceptBtn = cookieBox.querySelector('.buttons button')
 
 acceptBtn.onclick = ()=>{
-    //setting cookie for a month after one month it will expire
-    document.cookie = "CookieBy=Keddy; max-age="+60*60*24*30;
+    document.cookie = "CookieBy=Keddy; max-age="+60*60*24*30
     if(document.cookie){
-        cookieBox.classList.add("hide");
-    }
-    else{
-        alert("Cookie cannot be set");
+        cookieBox.classList.add('hide')
+    }else{
+        alert("cookie cannot be set")
     }
 }
-//check if the cookie has expired and show the cookie box
-let checkCookie = document.cookie.indexOf("CookieBy=Keddy")
-checkCookie != -1 ? cookieBox.classList.add("hide"): cookieBox.classList.remove("hide")
+let checkCookie = document.cookie.indexOf("CookieBy=Keddy");
+checkCookie!=-1?cookieBox.classList.add('hide'): cookieBox.classList.remove('hide')
